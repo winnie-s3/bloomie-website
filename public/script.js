c093361e7e42eb77bd9666002a75074345906f19
“Winnie <winniestefany303@gmail.com>
2023-05-11 22:59:07 -0300

    #

--- public/script.js
+++ public/script.js
@@ -1,7 +1,7 @@
-// $(window).on('load', function () {
-//     setTimeout(function () {
-//         $('#splash-screen').fadeOut('slow', function () {
-//             $(this).remove();
-//         });
-//     }, 2800);
-// });
+$(window).on('load', function () {
+    setTimeout(function () {
+        $('#splash-screen').fadeOut('slow', function () {
+            $(this).remove();
+        });
+    }, 2800);
+});
