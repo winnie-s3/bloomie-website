3350c06374d663253e77855cc8df6ea85a6c5894
“Winnie <winniestefany303@gmail.com>
2023-05-28 10:28:37 -0300

    create dna

--- public/script.js
+++ public/script.js
@@ -5,3 +5,24 @@
 //         });
 //     }, 2800);
 // });
+
+var swiper = new Swiper(".swiper-container", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  centeredSlides: true,
+  initialSlide: 1,
+  coverflowEffect: {
+    rotate: 0,
+    stretch: 0,
+    depth: 100,
+    modifier: 10,
+    initialSlide: 2,
+    slideShadows: true
+  },
+  pagination: {
+    el: ".swiper-pagination",
+     clickable: true
+  }
+});
