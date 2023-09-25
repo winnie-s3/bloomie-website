695ec392af9f7e634fb3e5fc39050201d881b39a
“Winnie <winniestefany303@gmail.com>
2023-09-25 16:12:01 -0300

    add api ibge no cadastro

--- public/script.js
+++ public/script.js
@@ -79,3 +79,34 @@ document.getElementById("btnPostagens").addEventListener("click", mostrarPostage
 
 // Mostrar as oportunidades por padrão ao carregar a página
 mostrarPostagens();
+
+// --------------- CIDADES E ESTADOS ------------------
+const ulrUF = 'https://servicodados.ibge.gov.br/api/v1/localidades/estados';
+const cidade = document.getElementById("cidade");
+const uf = document.getElementById("uf");
+
+uf.addEventListener('change', async function () {
+  const urlCidades = 'https://servicodados.ibge.gov.br/api/v1/localidades/estados/' + uf.value + '/municipios';
+  const request = await fetch(urlCidades);
+  const response = await request.json();
+  console.log(response.length);
+let options = ''
+  response.forEach(function (cidades) {
+    options += '<option>' + cidades.nome + '</option>'
+  });
+cidade.innerHTML = options;
+});
+
+window.addEventListener('load', async ()=> {
+  const request = await fetch(ulrUF)
+  const response = await request.json()
+
+  const options = document.createElement("optgroup")
+  options.setAttribute('label', 'UFS')
+  response.forEach(function(uf) {
+    options.innerHTML += '<option>' + uf.sigla + '</option>'
+})
+
+uf.append(options);
+
+})
