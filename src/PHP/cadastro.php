6114ba3e193046ce0dc2c0dc39e2fe09e7c739da
aylasantos <aylajuliassu@gmail.com>
2023-10-13 19:07:03 -0300

    debug do cadastro, ainda com erros

--- src/PHP/cadastro.php
+++ src/PHP/cadastro.php
@@ -1,26 +1,5 @@
 <?php
-// include('connect.php');
 
-    // if(isset($_POST['submit']))
-    // {
-
-    //     $nome = $_POST['nome']; 
-    //     $sobrenome = $_POST['sobrenome'];
-    //     $email = $_POST['email']; 
-    //     $usuario = $_POST['usuario']; 
-    //     $senha = $_POST['senha'];
-    //     // $estado = $_POST['estado'];
-    //     // $cidade = $_POST['cidade'];
-    //     $dia = $_POST['dia'];
-    //     $mes = $_POST['mes'];
-    //     $ano = $_POST['ano'];
-    //     $data_nasc = "$ano-$mes-$dia";
-
-    //     $result = mysqli_query($conexao, "INSERT INTO usuario(nome,sobrenome,senha,email,usuario,data_nasc) 
-    //     VALUES ('$nome','$sobrenome','$senha','$email','$usuario','$data_nasc')");
-
-    //     // header('Location: /src/PHP/cadastro.php');
-    // }
 include('connect.php');
 
 if(isset($_POST['submit']))
