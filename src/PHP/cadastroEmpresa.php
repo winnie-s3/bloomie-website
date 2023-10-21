1e62830e6fc5775fc6b9746639911f1e5b3a074c
aylasantos <aylajuliassu@gmail.com>
2023-10-21 19:24:18 -0300

    Eduarda - Alterações no cadastro do mentor e empresa no PHP

--- src/PHP/cadastroEmpresa.php
+++ src/PHP/cadastroEmpresa.php
@@ -1,18 +1,37 @@
+
 <?php
+
 include('connect.php');
 
-    if(isset($_POST['submit']))
+if(isset($_POST['submit']))
+{
+    // Verifique se todos os campos obrigatórios estão preenchidos
+    if(empty($_POST['setor_empresa']) || empty($_POST['email_empresa']) )
     {
-
-        $nomeEmpresa = $_POST['nomeEmpresa'];
-        $email = $_POST['email']; 
-        $senha = $_POST['senha'];
-        $setor = $_POST['setor'];
-
-        $result = mysqli_query($conexao, "INSERT INTO empresa(nome,sobrenome,senha,email,usuario,data_nasc,cidade,estado) 
-        VALUES ('$nome','$sobrenome','$senha','$email','$usuario','$data_nasc','$cidade','$estado')");
-
-        // header('Location: /src/PHP/cadastro.php');
+        echo "Todos os campos são obrigatórios!";
+        exit;
     }
 
+    // Obtenha os valores dos campos
+    $nome_empresa = $_POST['nome_empresa'];
+    $setor_empresa = $_POST['setor_empresa'];
+    $email_empresa = $_POST['email_empresa'];
+  
+
+    // Use declarações preparadas para evitar SQL Injection
+    $stmt = $conexao->prepare("INSERT INTO empresa(setor_empresa,email_empresa) VALUES ('$setor_empresa','$email_empresa')");
+
+
+    if ($stmt->execute()) {
+        echo "Cadastro realizado com sucesso!";
+        // Redirecione para outra página, se necessário
+        // header('Location: outra_pagina.php');
+    } else {
+        echo "Erro ao cadastrar: " . $stmt->error;
+    }
+
+    // Feche a declaração preparada e a conexão
+    $stmt->close();
+    $conexao->close();
+}
 ?>
