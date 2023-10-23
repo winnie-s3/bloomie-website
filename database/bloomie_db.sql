cfb0567aa5c0fd8a593149802219d25e166cf424
duardamessias <dumessiasdu@gmail.com>
2023-10-23 10:49:44 -0300

    atualização do banco de dados

--- database/bloomie_db.sql
+++ database/bloomie_db.sql
@@ -1,14 +1,16 @@
 -- --------------------------------------------------------
 -- Servidor:                     127.0.0.1
--- Versão do servidor:           5.1.72-community - MySQL Community Server (GPL)
--- OS do Servidor:               Win32
--- HeidiSQL Versão:              10.2.0.5599
+-- Versão do servidor:           5.7.33 - MySQL Community Server (GPL)
+-- OS do Servidor:               Win64
+-- HeidiSQL Versão:              11.2.0.6213
 -- --------------------------------------------------------
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 
 -- Copiando estrutura do banco de dados para bloomie_db
@@ -93,6 +95,7 @@ CREATE TABLE IF NOT EXISTS `curtidas` (
 CREATE TABLE IF NOT EXISTS `empresa` (
   `ID_empresa` int(11) NOT NULL,
   `ID_usuario` int(11) NOT NULL,
+  `nome_empresa` varchar(250) NOT NULL DEFAULT '',
   `setor_empresa` int(11) NOT NULL,
   `email_empresa` varchar(256) DEFAULT NULL,
   PRIMARY KEY (`ID_empresa`),
@@ -271,5 +274,6 @@ CREATE TABLE IF NOT EXISTS `usuario` (
 -- Exportação de dados foi desmarcado.
 
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
-/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
