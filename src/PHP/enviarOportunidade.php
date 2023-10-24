016e3822fe7a7d90866991ba1c194add31cf5b3b
aylasantos <aylajuliassu@gmail.com>
2023-10-24 15:27:06 -0300

    enviar oportunidade - funcionando

--- src/PHP/enviarOportunidade.php
+++ src/PHP/enviarOportunidade.php
@@ -1,25 +1,36 @@
 <?php
     include "connect.php";
+    
+if(isset($_POST['submit']))
    {
      $titulo = $_POST['titulo'];
-     $categoria = $_POST['categoria']; 
+    //$categoria = $_POST['categoria']; 
      $estado = $_POST['estado'];
      $cidade = $_POST['cidade'];
      $inicio = $_POST['inicio'];
      $tempo_expirar = $_POST['tempo_expirar'];
-     $descricao = $_POST['descricao'];
-     $escolaridade = $_POST['escolaridade'];
+     //$descricao = $_POST['descricao'];
+     //$escolaridade = $_POST['escolaridade'];
      $link = $_POST['link'];
      $tipo_personalidade = $_POST['tipo_personalidade'];
-     $tags = $_POST['tags'];
-     $imagem = $_POST['imagem'];
+     //$tags = $_POST['tags'];
+     // $imagem = $_POST['imagem'];
+ 
+      $stmt = $conexao->prepare("INSERT INTO oportunidade(titulo,estado,cidade,inicio,tempo_expirar,link,tipo_personalidade) VALUES ('$titulo','$estado','$cidade','$inicio','$tempo_expirar','$link','$tipo_personalidade')");
 
 
-     $result = mysqli_query($connect, "INSERT INTO oportunidade(titulo,categoria,estado,cidade,inicio,tempo_expirar,descricao,idade_min,idade_max,escolaridade,link,tipo_personalidade,tags,imagem) 
-     VALUES ('$titulo','$categoria','$estado','$cidade','$inicio','$tempo_expirar','$descricao','$idade_min','$idade_max','$escolaridade','$link','$tipo_personalidade','$tags','$imagem')");
+    
+      if ($stmt->execute()) {
+        echo "Cadastro realizado com sucesso!";
+        // Redirecione para outra página, se necessário
+        // header('Location: outra_pagina.php');
+    } else {
+        echo "Erro ao cadastrar: " . $stmt->error;
+    }
 
-     
-      echo"Dados Cadastrados";
+    // Feche a declaração preparada e a conexão
+    $stmt->close();
+    $conexao->close();
     
      }
 ?>
