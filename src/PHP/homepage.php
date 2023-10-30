109d26f237dcf90a0abd783b6c63d9abcd06985d
fernandamessias <fernanda.alves.messias@gmail.com>
2023-10-30 11:57:19 -0300

    updates homepage

--- src/PHP/homepage.php
+++ src/PHP/homepage.php
@@ -1,13 +1,14 @@
 <?php 
-include ('connect.php');
+
+include('connect.php');
 
 if(isset($_POST['submit'])){
-    $data_publicação = $_POST['data_publicacao'];
+    // $data_publicacao = $_POST['data_publicacao'];
     $documento = $_POST['documento'];
     $imagem = $_POST['imagem'];
     $texto = $_POST['texto'];
-    $stmt = $conexao->prepare("INSERT INTO post(data_publicacao,documento,imagem,texto) 
-    VALUES ('$data_publicaçao','$documento','$imagem','$texto')");
+    $stmt = $conexao->prepare("INSERT INTO post(documento,imagem,texto) 
+    VALUES ('$documento','$imagem','$texto')");
 
     if ($stmt->execute()) {
         echo "Post feito!";
